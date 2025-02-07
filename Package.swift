188ef9e2ab6e631ec3d89ff1fb4cfd5d13b8f506
Mikita Laptsionak <nikita.laptenok@gmail.com>
2025-02-07 16:51:02 +0100

    Update Package management files for version 2.7.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let version = "2.7.0"
 // Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
 let tag = "2.7.0"
-let checksum = "a1fb05c4fd6b73bc351f4d326e6171473b7a99b7f2bfa8f5f69f7281f66dc387"
+let checksum = "8fb0a7ecce1bce6aaad4c125a43c27657e9dec774f03cbf2a9603d3bb4e5ab57"
 let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
 let package = Package(
